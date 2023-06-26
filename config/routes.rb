6596b6bf9d79f39bbcbe51afdb084dcb13695fd8
futa4095 <kick20000@gmail.com>
2023-06-26 20:55:44 +0900

    ユーザー名を登録可能にした

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   root "root#index"
   get 'terms', to: "root#terms"
