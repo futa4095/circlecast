8998ba8015a55aa2c9261f59ca272151fc4e6b51
futa4095 <kick20000@gmail.com>
2023-06-30 14:01:08 +0900

    Groupモデルのバリデーションを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Group < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :description, length: { maximum: 1000 }
 end
