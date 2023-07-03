553edd6f110614561edaf5c825082249780dc130
futa4095 <kick20000@gmail.com>
2023-07-03 16:29:33 +0900

    ログイン後にグループ一覧へリダイレクトさせる

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@
 Rails.application.routes.draw do
   resources :groups
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
   }
 
   root "root#index"
