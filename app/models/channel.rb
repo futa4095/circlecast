257b8509bdf553cfa9d451024ff18b2183c15e36
futa4095 <kick20000@gmail.com>
2023-07-10 18:40:22 +0900

    Channelにバリデーションを追加

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -3,5 +3,8 @@
 class Channel < ApplicationRecord
   belongs_to :group
 
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :description, length: { maximum: 1000 }
+
   has_one_attached :artwork
 end
