ca971c63936f3cead4e6dfa02ac5a776f489d2e4
futa4095 <kick20000@gmail.com>
2023-07-10 20:18:24 +0900

    モデルの定義順を変更

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,9 +2,8 @@
 
 class Channel < ApplicationRecord
   belongs_to :group
+  has_one_attached :artwork
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :description, length: { maximum: 1000 }
-
-  has_one_attached :artwork
 end
