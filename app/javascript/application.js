d33a4292ff4397a62bf4dd1e09c6d2677106ab15
futa4095 <kick20000@gmail.com>
2023-08-06 09:43:17 +0900

    モーダルをAlpine.jsベースに変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,8 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-import "flowbite"
+import "alpine-turbo-drive-adapter"
+import Alpine from "alpinejs"
+
+window.Alpine = Alpine
+Alpine.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,5 +8,5 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 
-pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.0/flowbite.turbo.min.js"
-
+pin "alpinejs", to: "https://ga.jspm.io/npm:alpinejs@3.12.3/dist/module.esm.js"
+pin "alpine-turbo-drive-adapter", to: "https://ga.jspm.io/npm:alpine-turbo-drive-adapter@2.0.0/dist/alpine-turbo-drive-adapter.esm.js"
