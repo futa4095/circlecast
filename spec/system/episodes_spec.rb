73a22d2f2b663ca1c84f89bcdfe5de2c55a9d555
futa4095 <kick20000@gmail.com>
2023-08-17 11:18:42 +0900

    episodeのシステムスペックを追加

--- spec/system/episodes_spec.rb
+++ spec/system/episodes_spec.rb
@@ -2,18 +2,52 @@
 
 require 'rails_helper'
 
+HAVE_ENOUGH_DATA = '4'
+
 RSpec.describe 'Episodes', type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  fixtures :all
 
   describe '管理者の場合' do
-    it 'エピソードを作成すること'
-    it 'エピソードを削除すること'
+    before do
+      sign_in users(:nakajima)
+    end
+
+    it 'エピソードを作成すること' do
+      visit channel_path(channels(:group2_ch1))
+      click_on 'エピソードを作成'
+      fill_in 'タイトル', with: '新しいエピソードのタイトル！'
+      fill_in '説明', with: '新しいエピソードの説明です。新しいエピソードの説明その2です。'
+      attach_file '音声ファイル', 'spec/fixtures/files/audio1.m4a'
+      click_on '登録する'
+
+      expect(page).to have_content 'エピソードを作成しました'
+      expect(page).to have_content '新しいエピソードのタイトル！'
+      expect(page).to have_content '新しいエピソードの説明です。'
+    end
+
+    it 'エピソードを削除すること' do
+      visit channel_episode_path(channels(:nbc_channel2), episodes(:nbc_episode21))
+      click_on '削除'
+
+      expect(page).to have_content 'エピソードを削除しました'
+      expect(page).to have_no_content 'ep.1 2月2日'
+    end
   end
 
   describe 'メンバーの場合' do
-    it 'エピソードを表示すること'
+    before do
+      sign_in users(:nbc_student1)
+    end
+
+    it 'エピソードを表示すること' do
+      visit channel_path(channels(:nbc_channel2))
+      click_on 'ep.1 2月2日'
+
+      expect(page).to have_content 'ep.1 2月2日'
+      expect(page).to have_content '2023/08/04に公開'
+      audio = first('audio')
+      expect(audio[:readyState]).to eq HAVE_ENOUGH_DATA
+    end
     it 'エピソードを聴けること'
   end
 end
