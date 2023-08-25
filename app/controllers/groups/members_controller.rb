18408f42c5640462bd28dc7f6fe5bd3548db9265
futa4095 <kick20000@gmail.com>
2023-08-25 16:56:39 +0900

    グループ脱退可能にした

--- app/controllers/groups/members_controller.rb
+++ app/controllers/groups/members_controller.rb
@@ -10,8 +10,13 @@ module Groups
     end
 
     def update
-      @membership = @group.memberships.find_by!(user_id: params[:id])
-      @membership.update!(membership_params)
+      if self_leave?
+        @group.leave_member current_user
+        redirect_to groups_path, notice: "#{@group.name}から脱退しました"
+      else
+        @membership = @group.memberships.find_by!(user_id: params[:id])
+        @membership.update!(membership_params)
+      end
     end
 
     private
@@ -22,6 +27,10 @@ module Groups
 
     def membership_params
       params.require(:membership).permit(:id, :admin, :withdrawal)
+    end
+
+    def self_leave?
+      current_user.id == params[:id].to_i && membership_params[:withdrawal] == 'true'
     end
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -13,4 +13,12 @@ class Group < ApplicationRecord
   validates :icon, content_type: ['image/jpeg', 'image/png', 'image/webp'],
                    dimension: { width: { in: 300..3000 }, height: { in: 300..3000 } },
                    size: { between: (1.kilobyte)..(3.megabyte) }
+
+  # def add_member(user)
+  # end
+
+  def leave_member(user)
+    membership = memberships.find_by(user:)
+    membership.update!(withdrawal: true)
+  end
 end
