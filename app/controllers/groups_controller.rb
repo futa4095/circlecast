7ef3efa0092f17af6405aa9cd3ba5d2f4146c5e1
futa4095 <kick20000@gmail.com>
2023-08-28 13:38:48 +0900

    メンバー追加のリファクタ

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -57,9 +57,10 @@ class GroupsController < ApplicationController
 
   def join_group
     group_id = session[:group_to_join]
-    unless current_user.memberships.exists?(group_id:)
-      membership = current_user.memberships.create(group_id:)
-      flash.now[:notice] = "#{membership.group.name}に加入しました"
+    unless current_user.groups.exists?(group_id)
+      group = Group.find(group_id)
+      group.add_member current_user
+      flash.now[:notice] = "#{group.name}に加入しました"
     end
     session.delete :group_to_join
   end
