32eb3a8839c337b1ddeeb8ef45eb752f4090b9ce
futa4095 <kick20000@gmail.com>
2023-09-15 08:45:42 +0900

    管理者のみ可能なアクションに制限を追加

--- app/controllers/groups/members_controller.rb
+++ app/controllers/groups/members_controller.rb
@@ -4,6 +4,7 @@ module Groups
   class MembersController < ApplicationController
     before_action :authenticate_user!
     before_action :set_group
+    before_action :require_group_admin
 
     def index
       @memberships = @group.memberships.order(admin: :desc, id: :asc)
@@ -31,6 +32,12 @@ module Groups
 
     def self_leave?
       current_user.id == params[:id].to_i && membership_params[:withdrawal] == 'true'
+    end
+
+    def require_group_admin
+      return if @group.admin? current_user
+
+      redirect_back fallback_location: @group, alert: 'この操作を行うには、グループ管理者である必要があります'
     end
   end
 end
