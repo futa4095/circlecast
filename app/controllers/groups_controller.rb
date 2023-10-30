d5f74a0561f1ba0eb77501f2a87823ca69b50667
futa4095 <kick20000@gmail.com>
2023-10-30 21:24:47 +0900

    グループにページネーションを追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,7 @@ class GroupsController < ApplicationController
 
   def index
     join_group if session[:group_to_join].present?
-    @groups = current_user.groups.order(:created_at)
+    @groups = current_user.groups.order(:created_at).page(params[:page])
   end
 
   def show
