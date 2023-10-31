a71773a7045a482bb420b2ffdffb14953881aae9
futa4095 <kick20000@gmail.com>
2023-10-31 16:07:09 +0900

    メンバーにページネーションを追加

--- app/controllers/groups/members_controller.rb
+++ app/controllers/groups/members_controller.rb
@@ -7,7 +7,7 @@ module Groups
     before_action :require_group_admin, except: [:update]
 
     def index
-      @memberships = @group.memberships.order(admin: :desc, id: :asc)
+      @memberships = @group.memberships.order(admin: :desc, id: :asc).page(params[:page])
     end
 
     def update
