d7ea5e30bc73ebae13822a12c00378a7587049aa
futa4095 <kick20000@gmail.com>
2023-11-07 15:52:11 +0900

    resendの設定を記述

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "private_podcast_production"
 
+  config.action_mailer.delivery_method = :resend
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
