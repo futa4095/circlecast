90aa9ae22e9262e0dcfd14a0edd7aebd53f1354b
futa4095 <kick20000@gmail.com>
2023-12-04 14:42:13 +0900

    mailer senderに表示名を設定する

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'notice@circlecast.net'
+  config.mailer_sender = ActionMailer::Base.email_address_with_name 'no-reply@circlecast.net', 'CIRCLECAST'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
