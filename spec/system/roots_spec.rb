46ec0c69b539a52ecda07050fd3950f55bdebca4
futa4095 <kick20000@gmail.com>
2024-03-06 19:17:56 +0900

    トップページのテストを修正

--- spec/system/roots_spec.rb
+++ spec/system/roots_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Roots' do
   it 'トップページを表示すること' do
     visit root_path
-    expect(page).to have_content 'Circle Cast'
+    expect(page).to have_content 'CIRCLECAST'
   end
 
   it '利用規約を表示すること'
