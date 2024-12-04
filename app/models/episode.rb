2223a2afe08345c7a257a1e325b5448f0eb0cac7
futa4095 <kick20000@gmail.com>
2024-12-04 20:21:52 +0900

    active storage validationのアップデートに伴う変更

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -7,7 +7,7 @@ class Episode < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :description, length: { maximum: 1000 }
   validates :enclosure, attached: true,
-                        content_type: ['audio/mpeg', 'audio/mp4', 'video/mp4', 'video/quicktime'],
+                        content_type: ['audio/mpeg', 'audio/mp4', 'audio/x-m4a', 'video/mp4', 'video/quicktime'],
                         size: { between: (1.kilobyte)..(100.megabyte) }
 
   def updated?
